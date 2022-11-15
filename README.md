6c6890caee338c337f75ab132057096db535d5c7
Kevin O'Connor <kevin.oconnor@pilot.com>
2022-11-14 21:56:42 -0500

    Add README & stats page.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Link Cleaner
+This is a simple Flask service that will take a link and attempt to "clean" it. That means it will follow redirects to its eventual end, try and remove known tracker query parameters, amd maybe more.
+
+## Running
+```shell
+pip install -r requirements.txt
+cd app
+python -c "from db import CleanedLink, db; db.create_tables([CleanedLink])"
+flask run
+```
+
+## Using
+Just browse/send a GET request to `/clean/<url you want to clean>` and it'll return a clean link after a few seconds. Alternatively, send an `Accept: application/json` header and it will return a JSON dict with one key named `cleaned_url` containing the cleaned URL.
+
+You can also browse to `/stats` to see a beautifully designed stats page.
--- app/app.py
+++ app/app.py
@@ -1,12 +1,17 @@
 import json
 import logging
+import os
+import stat
 import time
 
+from datetime import datetime, timedelta
+from pathlib import Path
 from urllib.parse import urlparse, urlencode, urlunparse
 
 import validators
 
-from flask import Flask, request
+from flask import Flask, request, send_file
+from peewee import fn
 
 from cleaners import BaseCleaner
 from db import CleanedLink
@@ -48,3 +53,58 @@ def clean(url):
         )
     else:
         return f'<a href="{cleaned_url}">{cleaned_url}</a>'
+
+
+@app.route("/stats")
+def cached_stats():
+    if not os.path.exists("./stats.html"):
+        Path("./stats.html").touch()
+
+    file_stat = os.stat("./stats.html")
+    last_modified = time.time() - file_stat[stat.ST_MTIME]
+
+    if last_modified > 600 or file_stat[stat.ST_SIZE] == 0:
+        stats_html = stats()
+        with open("./stats.html", "w") as fh:
+            fh.write(stats_html)
+
+    return send_file("./stats.html")
+
+
+def stats():
+    one_day_ago = datetime.utcnow() - timedelta(days=1)
+    total_links = CleanedLink.select().count()
+    top_netlocs = list(
+        CleanedLink.select(
+            CleanedLink.netloc, fn.COUNT(CleanedLink.netloc).alias("count")
+        )
+        .group_by(CleanedLink.netloc)
+        .where(CleanedLink.created_at > one_day_ago)
+        .order_by("count")
+        .limit(10)
+        .execute()
+    )
+
+    top_list_items = []
+    for row in top_netlocs:
+        domain = row.netloc
+        count = row.count
+        top_list_items.append(f"<tr><td>{domain}</td><td>{count}</td></tr>")
+
+    top_list_html = "\n".join(top_list_items)
+
+    generation_time = datetime.utcnow()
+
+    return f"""
+    {total_links} links cleaned all time.<br><br>
+
+    Top domains of the last day:
+    <table>
+    <tr><th>Domain</th><th>Count</th></tr>
+    {top_list_html}
+    </table>
+    </ul><br>
+    <br>
+
+    <footer>Generated at {generation_time}.</footer>
+    """
